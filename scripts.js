da6345904d11d67253244b49a4fca95613da4ab7
Hugo Patrício <hugopatricio51@gmail.com>
2021-05-02 22:59:25 -0300

    feat adicionando o arquivo js com transacoes dinamicas

---
+++ scripts.js
@@ -0,0 +1,62 @@
+const Modal = {
+    open(){
+        document.querySelector('.modal-overlay').classList.add('active')
+    },
+    close(){
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: 50000,
+        date: '23/01/2021'
+    },
+    {
+        id: 2,
+        description: 'Internet',
+        amount: -70000,
+        date: '23/01/2021'
+    }
+]
+
+const Transaction = {
+    incomes(){
+
+    },
+    expenses(){
+
+    },
+    total(){
+
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        DOM.transactionsContainer.appendChild(tr)
+
+    },
+
+    innerHTMLTransaction(transaction){
+
+        const html = ` 
+        <tr>
+        <td class='description'>${transaction.description}</td>
+        <td class='expense'>${transaction.amount}</td>
+        <td class='date'>${transaction.date}</td>
+        <td> <img src="./assets/minus.svg" alt="Remover operação"></td>
+        </tr>`
+        return html
+    }
+}
+
+transactions.forEach(transaction =>{
+    DOM.addTransaction(transaction)
+})
